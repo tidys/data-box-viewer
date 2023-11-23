226ccb3a899198da5c4107f36e53fbdb147b4eee
xu_yanfeng <xu_yanfeng@126.com>
2023-11-23 18:08:48 +0800

    ok

--- src/App.vue
+++ src/App.vue
@@ -5,60 +5,64 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import type { TableColumn, TableData } from '@xuyanfeng/cc-ui/types/cc-table/const'
-import ccui from '@xuyanfeng/cc-ui'
-import axios from 'axios'
-const { CCTable } = ccui.components
+import { defineComponent, ref } from "vue";
+import type {
+  TableColumn,
+  TableData,
+} from "@xuyanfeng/cc-ui/types/cc-table/const";
+import ccui from "@xuyanfeng/cc-ui";
+import axios from "axios";
+const { CCTable } = ccui.components;
 
 interface LogData {
-  id: number
-  name: string
-  createDate: string
-  updateDate: string
+  id: number;
+  name: string;
+  createDate: string;
+  updateDate: string;
 }
 export default defineComponent({
-  name: 'app',
+  name: "app",
   components: {
-    CCTable
+    CCTable,
   },
   setup(props, ctx) {
     const columns = ref<TableColumn[]>([
       {
-        title: '日期',
-        key: 'time'
+        title: "日期",
+        key: "time",
+        width: 200,
       },
       {
-        title: '日志',
-        key: 'log'
-      }
-    ])
-    const data = ref<TableData[]>([])
+        title: "日志",
+        key: "log",
+      },
+    ]);
+    const data = ref<TableData[]>([]);
 
     // 获取服务器上的所有日志
-    const use = false
+    const use = true;
     if (use) {
-      const url = 'http://localhost:3000/log/get-all-log'
+      const url = "http://localhost:3000/log/get-all-log";
       axios.post(url).then((res) => {
-        console.log(res)
+        console.log(res);
         for (let i = 0; i < res.data.length; i++) {
-          const item: LogData = res.data[i]
-          data.value.push({ time: item.createDate, log: item.name })
+          const item: LogData = res.data[i];
+          data.value.push({ time: item.createDate, log: item.name });
         }
-      })
+      });
     } else {
       setTimeout(() => {
-        console.log('add')
-        // data.value.push({ time: '1', log: '1' })
-      }, 1000)
+        console.log("add");
+        data.value.push({ time: "1", log: "1" });
+      }, 1000);
     }
 
     return {
       data,
-      columns
-    }
-  }
-})
+      columns,
+    };
+  },
+});
 </script>
 
 <style scoped lang="less">
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-import '@xuyanfeng/cc-ui/dist/ccui.css'
-import '@xuyanfeng/cc-ui/iconfont/iconfont.css'
-import '@xuyanfeng/cc-ui/iconfont/use.css'
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import "@xuyanfeng/cc-ui/dist/ccui.css";
+import "@xuyanfeng/cc-ui/iconfont/iconfont.css";
+import "@xuyanfeng/cc-ui/iconfont/use.css";
+createApp(App).mount("#app");
